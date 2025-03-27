caa64e7f86ecb00bf53b09c596f193a90cc9f95f
Taiyo200 <petrusvlad01@gmail.com>
2025-03-27 16:39:28 +0200

    Added game window

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    print(f"Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     
+    pygame.init
+    
+    if pygame.init:
+        print("Pygame Initialized")
+        
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), vsync=1)
+    
+    while(True):
+        pygame.Surface.fill(screen, (0,0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.flip()
+        
 if __name__ == "__main__":
     main()
