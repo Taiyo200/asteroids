878e4a9e40d18653560493615a9182563cd1d097
Taiyo200 <petrusvlad01@gmail.com>
2025-03-27 17:55:30 +0200

    Added the player and it's hitbox

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     print(f"Starting Asteroids!")
@@ -13,12 +15,23 @@ def main():
         
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), vsync=1)
     
+    dt = 0
+    game_clock = pygame.time.Clock()
+    
+    player = Player (x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    
     while(True):
         pygame.Surface.fill(screen, (0,0,0))
+        
+        player.draw(screen)
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.display.flip()
+        
+        dt = game_clock.tick(60) / 1000
+        
         
 if __name__ == "__main__":
     main()
